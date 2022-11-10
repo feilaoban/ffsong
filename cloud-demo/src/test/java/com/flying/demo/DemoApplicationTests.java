cc2b3383aec12361b391f5c53beb419ee79a314c
songfeifei <18767153679@163.com>
2022-11-10 18:08:08 +0800

    策略模式消除if-else

--- cloud-demo/src/test/java/com/flying/demo/DemoApplicationTests.java
+++ cloud-demo/src/test/java/com/flying/demo/DemoApplicationTests.java
@@ -1,10 +1,17 @@
 package com.flying.demo;
 
+import com.flying.demo.strategy.AbstractOrderProcess;
 import com.flying.demo.strategy.CheckStoreProcess;
+import com.flying.demo.strategy.GenerateOrderProcess;
+import com.flying.demo.strategy.ReduceStoreProcess;
 import com.flying.demo.strategy.handler.OrderProcessHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -40,7 +47,17 @@ class DemoApplicationTests {
 
     @Test
     void OrderProcessTest1() {
-        CheckStoreProcess checkStore = new CheckStoreProcess();
-        System.out.println(checkStore.onSaveSuccess());  // CheckStore - 校验库存
+        Map<String, AbstractOrderProcess> map = new HashMap<>();
+        map.put("1", new CheckStoreProcess());
+        map.put("2", new GenerateOrderProcess());
+        map.put("3", new ReduceStoreProcess());
+
+        String key = "1";
+        // 可通过给每种策略预定义编码的方式，通过编码调用对应的方法，从而消除 if-else
+        AbstractOrderProcess process = map.get(key);
+        if (Objects.isNull(process)) {
+            throw new RuntimeException("策略不符合");
+        }
+        System.out.println(process.onSaveSuccess());  // CheckStore - 校验库存
     }
 }

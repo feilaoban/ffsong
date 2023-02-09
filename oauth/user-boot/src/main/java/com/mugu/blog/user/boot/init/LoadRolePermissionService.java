1bc4b8913783f50e9b45d2013ddc51622f13b753
songfeifei <18767153679@163.com>
2023-02-09 16:10:57 +0800

    新增 @PostConstruct 使用说明

--- oauth/user-boot/src/main/java/com/mugu/blog/user/boot/init/LoadRolePermissionService.java
+++ oauth/user-boot/src/main/java/com/mugu/blog/user/boot/init/LoadRolePermissionService.java
@@ -24,6 +24,8 @@ public class LoadRolePermissionService {
 
     /**
      * 初始化权限<->角色对应关系，将其放置到Redis中
+     * 注：@PostConstruct注解的方法在项目启动的时候执行这个方法，也可以理解为在spring容器启动的时候执行，可作为一些数据的常规化加载，比如数据字典之类的。
+     * Constructor >> @Autowired >> @PostConstruct
      */
     @PostConstruct
     public void init(){

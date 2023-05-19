13ede95772aecccbb533c8221690f94279cc19dd
songfeifei <18767153679@163.com>
2023-05-19 11:07:08 +0800

    更新注释

--- demo/cloud-demo/src/main/java/com/flying/demo/handler/event/CustomSpringEventListener.java
+++ demo/cloud-demo/src/main/java/com/flying/demo/handler/event/CustomSpringEventListener.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 /**
  * @Author songfeifei
  * @Date 2023/5/12 15:11
- * @Description 事件监听器
+ * @Description 事件监听器，观察者模式解耦的一种实现
  */
 @Component
 public class CustomSpringEventListener implements ApplicationListener<CustomSpringEvent> {
